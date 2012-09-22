282bd02d3d6b08af6d16c5aac9e36fb3448863b9
Nick Barth <nickbarth@live.com>
2012-09-22 17:32:11 +0000

    fixed rake tasks

--- lib/rake_assets/rake.rb
+++ lib/rake_assets/rake.rb
@@ -2,4 +2,27 @@ require 'bundler'
 Bundler.require
 
 require 'rake_assets'
-import 'rake_assets/compile_tasks.rake'
+
+namespace :assets do
+  desc 'compile assets'
+  task :compile => [:check, :compile_js, :compile_css] do
+  end
+
+  desc 'check config'
+  task :check do
+    @rake_assets ||= RakeAssets.new
+    @rake_assets.check
+  end
+
+  desc 'compile javascript'
+  task :compile_js do
+    @rake_assets ||= RakeAssets.new
+    @rake_assets.compile_js
+  end
+
+  desc 'compile css'
+  task :compile_css do
+    @rake_assets ||= RakeAssets.new
+    @rake_assets.compile_css
+  end
+end
